b633aa910a51f813bdaf56d278eff3f45186cae8
704252630@qq.com <704252630@qq.com>
2022-09-05 11:32:36 +0800

    测试git

--- cartographer_detailed_comments_ws-master/src/cartographer_ros/cartographer_ros/cartographer_ros/node_main.cc
+++ cartographer_detailed_comments_ws-master/src/cartographer_ros/cartographer_ros/cartographer_ros/node_main.cc
@@ -21,7 +21,7 @@
 #include "cartographer_ros/ros_log_sink.h"
 #include "gflags/gflags.h"
 #include "tf2_ros/transform_listener.h"
-// jc:test git
+// jc:test git asdasdasd 火车车次
 /**
  * note: gflags是一套命令行参数解析工具
  * DEFINE_bool在gflags.h中定义
