d18f602997a4b1777b67a26c86b527f29dd9446d
704252630@qq.com <704252630@qq.com>
2022-09-05 12:10:19 +0800

    as

--- cartographer_detailed_comments_ws-master/src/cartographer_ros/cartographer_ros/cartographer_ros/node_main.cc
+++ cartographer_detailed_comments_ws-master/src/cartographer_ros/cartographer_ros/cartographer_ros/node_main.cc
@@ -21,7 +21,7 @@
 #include "cartographer_ros/ros_log_sink.h"
 #include "gflags/gflags.h"
 #include "tf2_ros/transform_listener.h"
-// jc:test git 测试23333
+// jc:test git 测试目的yayayay
 /**
  * note: gflags是一套命令行参数解析工具
  * DEFINE_bool在gflags.h中定义
